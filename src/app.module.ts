27351922cd17232fc459abc13a27fbd9af6f340c
Carlo Roberto <crgmzlps@gmail.com>
2021-07-03 17:42:23 -0300

    db cfg & entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 
+const mysqlCfg: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'db', //docker-compose service
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'admin',
+  autoLoadEntities: true,
+  synchronize: true,
+};
+
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot(mysqlCfg), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './models/user.entity';
 import { UserController } from './user.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
 })
 export class UserModule {}
