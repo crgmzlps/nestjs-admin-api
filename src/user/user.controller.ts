0cbb1432fd6b782365ed2f7144d0d82f97ef89f6
Carlo Roberto <crgmzlps@gmail.com>
2021-07-03 18:35:19 -0300

    services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from './models/user.entity';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get()
-  all() {
-    return ['john', 'jane'];
+  async all(): Promise<User[]> {
+    return await this.userService.all();
   }
 }
