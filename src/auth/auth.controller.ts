9da815245bab7c54132498e43d898d090c7cef2b
Carlo Roberto <crgmzlps@gmail.com>
2021-07-03 19:02:30 -0300

    hashing passwords

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Controller('auth')
 export class AuthController {
   constructor(private userService: UserService) {}
   @Post('register')
   async register(@Body() body) {
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+    body.password = hashedPassword;
     return await this.userService.create(body);
   }
 }
